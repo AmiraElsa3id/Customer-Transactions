0de462fdee40c6cd28b6f42405488198ec74e9f3
AmeraElsa3id <ameraelsa3id@gmail.com>
2024-07-17 19:35:27 +0300

    modified:   css/style.css modified:   index.html modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -22,10 +22,8 @@ console.log(detailsBtns)
 
 async function display(customers, transactions){
     displayChart(1);
-// $('data').html()
 let box=``;
 let details=``;
-// const customerTransactions = transactions.filter(t => t.customer_id === customer.id);
 customers.forEach(function(customer){
     const customerTransactions = transactions.filter(t => t.customer_id === customer.id);
     let dates =customerTransactions.map(t => t.date);
@@ -129,109 +127,5 @@ detailsBtns.forEach(btn => {
 })
 });
 
-// document.querySelector(".details-bt").forEach(function(btn){
-//     btn.addEventListener("click",function(e){
-//         e.preventDefault();
-//         console.log(btn)
-//     })
-// })
-// $(".details-btn").click(function(e){
-//     console.log(e)
-//     $(".details-btn").not($(this)).next().slideUp()
-//     $(this).next().slideToggle()
-// }
-// )
-// $('.details-btn').on('click', function (event) {
-//     event.preventDefault();
-//     $(this).closest('.customer-data').find('.details').toggle();
-// });
 
-// $('.details-btn').on("click",function(event){
-//     console.log("it work")
-//     event.preventDefault();
-//         this.next().slideToggle(300);
-// })
-// function displayDetails(){
-// $('.details').eq(this).next().slideToggle(300);
-// console.log(this)
-// }
-
-// const displayCustomers = (customers, transactions) => {
-//     customerTableBody.innerHTML = '';
-//     customers.forEach(customer => {
-//         const customerTransactions = transactions.filter(t => t.customer_id === customer.id);
-//         const totalAmount = customerTransactions.reduce((sum, t) => sum + t.amount, 0);
-//         const row = document.createElement('tr');
-//         row.innerHTML = `<td>${customer.name}</td><td colspan="2">${totalAmount}</td>`;
-//         row.addEventListener('click', () => {
-//             displayTransactions(customerTransactions);
-//             displayChart(customerTransactions);
-//         });
-//         customerTableBody.appendChild(row);
-//     });
-// };
-
-// const displayTransactions = (customerTransactions) => {
-//     customerTransactions.forEach(transaction => {
-//         const transactionRow = document.createElement('tr');
-//         transactionRow.classList.add('transaction-row');
-//         transactionRow.innerHTML = `<td></td><td>${transaction.date}</td><td>${transaction.amount}</td>`;
-//         customerTableBody.appendChild(transactionRow);
-//     });
-// };
-
-// const displayChart = (customerTransactions) => {
-//     const dates = customerTransactions.map(t => t.date);
-//     const amounts = customerTransactions.map(t => t.amount);
-
-//     if (chart) {
-//         chart.destroy();
-//     }
-
-//     chart = new Chart(transactionChartCtx, {
-//         type: 'line',
-//         data: {
-//             labels: dates,
-//             datasets: [{
-//                 label: 'Transaction Amount',
-//                 data: amounts,
-//                 borderColor: 'rgba(75, 192, 192, 1)',
-//                 borderWidth: 1,
-//                 fill: false
-//             }]
-//         },
-//         options: {
-//             scales: {
-//                 x: {
-//                     type: 'time',
-//                     time: {
-//                         unit: 'day'
-//                     }
-//                 }
-//             }
-//         }
-//     });
-// };
-
-// filterInput.addEventListener('input', (e) => {
-//     const filterValue = e.target.value.toLowerCase();
-//     const filteredCustomers = customers.filter(customer => 
-//         customer.name.toLowerCase().includes(filterValue)
-//     );
-//     const filteredTransactions = transactions.filter(transaction => 
-//         transaction.amount.toString().includes(filterValue)
-//     );
-
-//     const combinedFilteredCustomers = customers.filter(customer => 
-//         filteredTransactions.some(transaction => transaction.customer_id === customer.id) || 
-//         customer.name.toLowerCase().includes(filterValue)
-//     );
-
-//     displayCustomers(combinedFilteredCustomers, transactions);
-// });
-
-
-
-
-
-getData()
+getData();
